ae4f6a2740bb392e155634cd63f0a444d8da30f8
Matthew Yee <myee@redhat.com>
2025-05-29 06:17:57 -0700

    added cockpit functionality

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -5,3 +5,8 @@
 dnf downgrade openssh -y
 systemctl restart sshd.service
 
+# Enable cockpit functionality in showroom.
+echo "[WebService]" > /etc/cockpit/cockpit.conf
+echo "Origins = https://cockpit-$(hostname -f|cut -d"-" -f2).apps.$(grep search /etc/resolv.conf| grep -o '[^ ]*$')" >> /etc/cockpit/cockpit.conf
+echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
+systemctl enable --now cockpit.socket
